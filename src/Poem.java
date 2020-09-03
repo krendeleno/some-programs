c932ac10b10169ef32af1f03367c98438f7eb342
krendeleno <krendeleno@gmail.com>
2020-09-03 17:07:43 +0300

    Сделаны хард и изи проверки

--- src/Poem.java
+++ src/Poem.java
@@ -1,4 +1,7 @@
 public class Poem {
+    static char[] vowels = new char[]{'a', 'o', 'u', 'y', 'i', 'e'};
+    static char[] commas = new char[]{';', ',', '?', '!', '.', ':'};
+
     String[] poemStrings;
 
     Poem(String[] poemStrings) {
@@ -14,5 +17,32 @@ public class Poem {
                             System.out.println("Строка " + (i + 1) + " рифмуется со строкой " + (j + 1));
                 }
 
+    }
+
+    public void findRhymesHard() {
+        int iVowel = -1;
+        int jVowel = -1;
+        for (int i = 0; i < poemStrings.length - 1; i++) {
+            for (char vowel : vowels)
+                if (poemStrings[i].lastIndexOf(vowel) != -1 && poemStrings[i].lastIndexOf(vowel) > iVowel) {
+                    iVowel = poemStrings[i].indexOf(vowel);
+                }
+            if (iVowel != -1) {
+                String iString = poemStrings[i].replaceAll("[^a-zA-Z ]", "");
+                for (int j = i + 1; j < poemStrings.length; j++) {
+                    for (char vowel : vowels)
+                        if (poemStrings[j].lastIndexOf(vowel) != -1 && poemStrings[j].lastIndexOf(vowel) > jVowel) {
+                            jVowel = poemStrings[j].lastIndexOf(vowel);
+                        }
+                    if (jVowel != -1) {
+                        String jString = poemStrings[j].replaceAll("[^a-zA-Z ]", "");
+                        if (iString.substring(iVowel).equals(jString.substring(jVowel)))
+                            System.out.println("Строка " + (i + 1) + " рифмуется со строкой " + (j + 1));
+                    }
+                    jVowel = -1;
+                }
+            }
+            iVowel = -1;
+        }
     }
 }
--- src/TestPoem.java
+++ src/TestPoem.java
@@ -1,6 +1,8 @@
 public class TestPoem {
     public static void main(String[] args) {
         Poem a = new Poem (new String[] {"Hello", "Sprinter", "Sr", "Autumter"});
+        Poem b = new Poem (new String[] {"Hello", "Sprinter,", "Sr", "Autumter,"});
         a.findRhymesSimple();
+        b.findRhymesHard();
     }
 }
